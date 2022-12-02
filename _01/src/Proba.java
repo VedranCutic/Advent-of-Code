929280892f56377367001bccc185dcaca4bfe633
Bedran <117116794+VedranCutic@users.noreply.github.com>
2022-12-02 18:07:19 +0100

    Delete _01 directory

--- _01/src/Proba.java
+++
@@ -1,9 +0,0 @@
-public class Proba {
-    public static void main(String[] args) {
-        System.out.println("hi, this swhit works");
-        System.out.println("wow");
-
-
-        System.out.println("is this good");
-    }
-}
