b4f6f691828c5c7db2dc96e816247f517c7cb53b
Vedran Cutic <vc54146@fer.hr>
2024-12-01 15:13:13 +0100

    AOC 2024: Day 01.

---
+++ 2024/Day01/first.py
@@ -0,0 +1,42 @@
+input = []
+
+with open("input.txt", "r") as input_file:
+    input = input_file.readlines()
+
+left_list = []
+right_list = []
+
+for line in input:
+    tmp = line.split("   ")
+    left_list.append(int(tmp[0]))
+    right_list.append(int((tmp[1]).strip()))
+
+left_list = sorted(left_list)
+right_list = sorted(right_list)
+
+solution = 0
+for i in range(len(left_list)):
+    solution += abs(left_list[i] - right_list[i])
+
+print(solution)
+
+
+similarity = 0
+left_list_dict = {}
+right_list_dict = {}
+
+for i in range(len(left_list)):
+    if left_list[i] not in left_list_dict:
+        left_list_dict[left_list[i]] = 1
+    else:
+        left_list_dict[left_list[i]] += 1
+    if right_list[i] not in right_list_dict:
+        right_list_dict[right_list[i]] = 1
+    else:
+        right_list_dict[right_list[i]] += 1
+
+for i in range(len(left_list)):
+    if left_list[i] in right_list_dict:
+        similarity += left_list[i] * right_list_dict[left_list[i]]
+
+print(similarity)
