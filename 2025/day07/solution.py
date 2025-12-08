fae1a50c3b78cd5f6bcc68c60d20c171c3114588
vedran.cutic <vedran.cutic@infigo.is>
2025-12-08 14:50:58 +0100

    AOC 2025: Day07 solution.2

--- 2025/day07/solution.py
+++ 2025/day07/solution.py
@@ -11,21 +11,18 @@ def beam_emmition(beams, manifold):
     return splits + beam_emmition(beams=beams, manifold=manifold[1:])
 
 
-def beam_timelines(beam, manifold):
-    new_beams = []
-    if not manifold:
-        return 1
-
-    if manifold[0][beam] == "^":
-        new_beams.append(beam - 1)
-        new_beams.append(beam + 1)
-        tmp = 0
-        for b in new_beams:
-            tmp += beam_timelines(b, manifold[1:])
-        return tmp
-
-    else:
-        return beam_timelines(beam, manifold[1:])
+def beam_timelines(manifold):
+    new_beams = [0] * len(manifold[0])
+    new_beams[manifold[0].index("S")] = 1
+    result = 1
+    for i in range(1, len(manifold)):
+        for j, location in enumerate(manifold[i]):
+            if location == "^" and new_beams[j] > 0:
+                new_beams[j - 1] += new_beams[j]
+                new_beams[j + 1] += new_beams[j]
+                result += new_beams[j]
+                new_beams[j] = 0
+    return result
 
 
 def main():
@@ -39,11 +36,10 @@ def main():
     for i in range(length):
         if input[0][i] == "S":
             beams.add(i)
-            beam = i
             break
     tachyon_splits = beam_emmition(beams=beams, manifold=input[1:])
-    timelines = beam_timelines(beam=beam, manifold=input[1:])
     print(f"SOLUTION PART 1: {tachyon_splits}")
+    timelines = beam_timelines(manifold=input)
     print(f"SOLUTION PART 2: {timelines}")
 
 
