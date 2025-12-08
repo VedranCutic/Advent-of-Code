c751c8b472d5ae85bba4c8debcc353922f8231b4
vedran.cutic <vedran.cutic@infigo.is>
2025-12-08 15:42:39 +0100

    AOC 2025: Add Day07 visualization.2

--- 2025/day07/solution.py
+++ 2025/day07/solution.py
@@ -1,3 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import seaborn as sns
+from matplotlib.colors import BoundaryNorm, ListedColormap, LogNorm
+
+
 def beam_emmition(beams, manifold):
     if not manifold:
         return 0
@@ -12,8 +19,13 @@ def beam_emmition(beams, manifold):
 
 
 def beam_timelines(manifold):
+    matrices_visualization = []
+    tmp = np.zeros((len(manifold), len(manifold[0])))
+    matrices_visualization.append(tmp.copy())
     new_beams = [0] * len(manifold[0])
     new_beams[manifold[0].index("S")] = 1
+    tmp[0] = np.array(new_beams)
+    matrices_visualization.append(tmp.copy())
     result = 1
     for i in range(1, len(manifold)):
         for j, location in enumerate(manifold[i]):
@@ -22,7 +34,39 @@ def beam_timelines(manifold):
                 new_beams[j + 1] += new_beams[j]
                 result += new_beams[j]
                 new_beams[j] = 0
-    return result
+        tmp[i] = np.array(new_beams)
+        matrices_visualization.append(tmp.copy())
+    return result, matrices_visualization
+
+
+def create_gif(matrices):
+
+    matrices = [np.array(m, dtype=float) + 1e-10 for m in matrices]
+    fig, ax = plt.subplots(facecolor="#0d1117")
+    cmap = "cividis"
+    sns.heatmap(
+        matrices[0], cmap=cmap, norm=LogNorm(), cbar=False
+    )  # or "plasma", "magma", "cividis"
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    def update(frame):
+        ax.clear()
+        sns.heatmap(
+            matrices[frame],
+            cmap=cmap,
+            norm=LogNorm(),
+            cbar=False,
+            square=True,
+            ax=ax,
+        )
+        ax.set_xticks([])
+        ax.set_yticks([])
+        return (ax,)
+
+    ani = animation.FuncAnimation(fig, update, frames=len(matrices), blit=False)
+
+    ani.save("day07.gif", writer="pillow", fps=25)
 
 
 def main():
@@ -39,8 +83,9 @@ def main():
             break
     tachyon_splits = beam_emmition(beams=beams, manifold=input[1:])
     print(f"SOLUTION PART 1: {tachyon_splits}")
-    timelines = beam_timelines(manifold=input)
+    timelines, matrices_vis = beam_timelines(manifold=input)
     print(f"SOLUTION PART 2: {timelines}")
+    create_gif(matrices_vis)
 
 
 if __name__ == "__main__":
