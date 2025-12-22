82d37d6116cd4975337c50c389be84755405385b
vedran.cutic <vedran.cutic@infigo.is>
2025-12-22 10:49:52 +0100

    AOC 2025: Day08.

--- 2025/day08/solution.py
+++ 2025/day08/solution.py
@@ -1,23 +1,88 @@
 import math
+import itertools
+
+MAX_CONNECTIONS = 10
+
+
+# collection of all the points keyed to their x,y,z values
+def create_collection(input: list):
+    collection = {}
+    for i in range(len(input)):
+        collection[i] = input[i]
+    return collection
+
 
 def calculate_Euclidean_distance(point1, point2):
-    return math.sqrt(sum((point1[i] - point2[i])**2 for i in range(3)))
+    return math.sqrt(sum((point1[i] - point2[i]) ** 2 for i in range(3)))
+
 
 def main():
-    filename = "tmp.txt"
+    filename = "input.txt"
     with open(filename, "r") as file:
         input = file.readlines()
     input = [list(map(int, line.strip().split(","))) for line in input]
-    
-    smallest = 1e10
-    for point1 in input:
-        for point2 in input:
-            if point1 == point2:
-                continue
-            a = calculate_Euclidean_distance(point1, point2)
-            if a < smallest:
-                min = (point1, point2)
-    print(min)
+
+    collection = create_collection(input)
+    combinations = itertools.combinations(collection, 2)
+
+    # this is a list that saves the smalles distance between two points
+    euclidean_distances = []
+    for c in combinations:
+        hash1, hash2 = c
+        p1 = collection[hash1]
+        p2 = collection[hash2]
+        dist = calculate_Euclidean_distance(p1, p2)
+        euclidean_distances.append((dist, hash1, hash2))
+    euclidean_distances_sorted = sorted(euclidean_distances, key=lambda x: x[0])
+
+    connections = []
+    used_points = set()
+    for connection in euclidean_distances_sorted[:MAX_CONNECTIONS]:
+        # start
+        if not connections:
+            p1, p2 = connection[1:]
+            used_points.add(p1)
+            used_points.add(p2)
+            connections.append({p1, p2})
+
+        else:
+            p1, p2 = connection[1:]
+            if p1 in used_points or p2 in used_points:
+                for i, c in enumerate(connections):
+                    if p1 in c or p2 in c:
+                        connections[i].add(p1)
+                        connections[i].add(p2)
+                        used_points.add(p1)
+                        used_points.add(p2)
+            else:
+                used_points.add(p1)
+                used_points.add(p2)
+                connections.append({p1, p2})
+
+    # some circuits(sets) have same boxes and they should be connected
+    # into one new circuit
+    merged = []
+    found = True
+    while found:
+        for circuit1 in connections:
+            found = None
+            for circuit2 in merged:
+                if circuit1 & circuit2:
+                    circuit2 |= circuit1
+                    found = circuit2
+                    break
+
+            if not found:
+                merged.append(circuit1)
+            # transitive overlap
+
+    sizes = []
+    for c in merged:
+        sizes.append(len(c))
+
+    print(sizes)
+    print(f"SOLUTION PART1: {math.prod(sizes)}")
+
 
 if __name__ == "__main__":
     main()
